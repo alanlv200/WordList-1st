531037704f5ed89fd6cf2b5f54281e7cc809079f
alanlv200 <vistlv200@gmail.com>
2016-02-11 09:49:27 +0900

    Initial Commit

---
+++ WordList 2ndTests/WordList_2ndTests.swift
@@ -0,0 +1,36 @@
+//
+//  WordList_2ndTests.swift
+//  WordList 2ndTests
+//
+//  Created by 平山大翼 on 2016/02/11.
+//  Copyright © 2016年 平山大翼. All rights reserved.
+//
+
+import XCTest
+@testable import WordList_2nd
+
+class WordList_2ndTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
